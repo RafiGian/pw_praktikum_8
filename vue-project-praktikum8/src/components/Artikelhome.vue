41dfac030577750b72f1643b00de9fe237533d37
Rafi Gian <rafigian28@gmail.com>
2024-05-30 17:09:38 +0700

    praktikum 8

---
+++ vue-project-praktikum8/src/components/Artikelhome.vue
@@ -0,0 +1,9 @@
+<template>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+        <h1>ini adalah halaman artikel</h1>
+        <p>artikel wkwkwkwk</p>
+        <p>artikel wkwkwkwk</p>
+        <p>artikel wkwkwkwk</p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Artikelhome from '../components/Artikelhome.vue';
+</script>
+
+<template>
+  <main>
+    <Artikelhome />
+  </main>
+</template>
